05b37fd64bcf21a1b7d50fd5a66a5d12180da1a6
Tyler Schaffer <tpschaffer@gmail.com>
2019-11-04 10:27:53 -0700

    Update google service spec to test finding an address from coords

--- spec/services/google_service_spec.rb
+++ spec/services/google_service_spec.rb
@@ -4,6 +4,8 @@ describe GoogleService do
   before(:each) do
     @location_1 = Location.create(address: "Denver,CO")
     @google_service_1 = GoogleService.new(@location_1)
+    @location_2 = Location.create(latitude: 35.675, longitude: -47.893)
+    @google_service_2 = GoogleService.new(@location_2)
   end
 
   it "exists" do
@@ -23,6 +25,21 @@ describe GoogleService do
       coordinates = @google_service_1.coordinates
       expect(coordinates[:lat]).to eq 39.7392358
       expect(coordinates[:lng]).to eq -104.990251
+    end
+  end
+
+  it "#get_json_address" do
+    VCR.use_cassette("google_service_raw_address") do
+      json_response = @google_service_2.get_json_address
+      expect(json_response[:status]).to eq "OK"
+      expect(json_response[:results][0][:formatted_address]).to eq "Atlantic Ocean"
+    end
+  end
+
+  it "#address" do
+    VCR.use_cassette("google_service_address") do
+      address = @google_service_2.address
+      expect(address).to eq "Atlantic Ocean"
     end
   end
 end
