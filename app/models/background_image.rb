aff22df787a15314397e82e3361847d5a5574514
Tyler Schaffer <tpschaffer@gmail.com>
2019-11-03 22:06:08 -0700

    Add background image model

---
+++ app/models/background_image.rb
@@ -0,0 +1,3 @@
+class BackgroundImage < ApplicationRecord
+  belongs_to :location
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_04_033808) do
+ActiveRecord::Schema.define(version: 2019_11_04_040034) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "background_images", force: :cascade do |t|
+    t.bigint "location_id"
+    t.string "description"
+    t.string "alt_description"
+    t.string "raw_url"
+    t.string "full_url"
+    t.string "city"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["location_id"], name: "index_background_images_on_location_id"
+  end
 
   create_table "locations", force: :cascade do |t|
     t.string "address"
@@ -23,4 +35,5 @@ ActiveRecord::Schema.define(version: 2019_11_04_033808) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "background_images", "locations"
 end
