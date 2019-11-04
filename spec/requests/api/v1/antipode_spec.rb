6d05548de332c423a254a0335317c34e5f1a5c99
Tyler Schaffer <tpschaffer@gmail.com>
2019-11-04 10:30:35 -0700

    Test antipode request

---
+++ spec/requests/api/v1/antipode_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe "GET /api/v1/antipodes" do
+  it "provides an antipode for a specific city" do
+    VCR.use_cassette("antipode_by_location") do
+      get '/api/v1/antipodes?location=hong kong'
+
+      expect(response).to be_successful
+
+      data = JSON.parse(response.body)
+      keys = [
+          "id",
+          "search_city",
+          "antipode_city",
+          "forecast"
+      ]
+      expect(data["data"]["attributes"].keys).to eq(keys)
+    end
+  end
+end
