b633a21c2619803b4ad5e2e5f98150adc4632e9e
Tyler Schaffer <tpschaffer@gmail.com>
2019-11-03 22:06:55 -0700

    Rename controllers and set up image controller show method

--- app/controllers/api/v1/image_controller.rb
+++
@@ -1,9 +0,0 @@
-class Api::V1::ImageController < ApplicationController
-  def show
-    # check params if the location/image already exists
-    # connect to unsplash api
-    # get json from unsplash
-    # make image object in db
-    # render json
-  end
-end
