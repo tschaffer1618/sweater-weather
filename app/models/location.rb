70cda5e3dafd0b4560f7f37f08736ae7c861ebaa
Tyler Schaffer <tpschaffer@gmail.com>
2019-11-04 09:56:28 -0700

    Remove verification from location so it can be created with address or coords

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,5 +1,3 @@
 class Location < ApplicationRecord
   has_one :background_image
-
-  validates_presence_of :address
 end
--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -4,8 +4,4 @@ describe Location do
   describe "relationships" do
     it {should have_one :background_image}
   end
-
-  describe "validations" do
-    it {should validate_presence_of :address}
-  end
 end
