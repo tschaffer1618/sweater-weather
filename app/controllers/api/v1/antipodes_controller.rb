8b6a9e4980f764ba40db6f622412a1a7d7fedb3a
Tyler Schaffer <tpschaffer@gmail.com>
2019-11-04 11:03:54 -0700

    Second refactor to utilize an antipode facade

--- app/controllers/api/v1/antipodes_controller.rb
+++ app/controllers/api/v1/antipodes_controller.rb
@@ -1,35 +1,7 @@
 class Api::V1::AntipodesController < ApplicationController
   def show
-    search_location = update_search_location
-    antipode_coords = AmypodeService.new(search_location).get_antipode_coords
-    antipode_location = update_antipode_location(antipode_coords)
-    forecast_hash = DarkSkyService.new(antipode_location).get_json_forecast
-    forecast = Forecast.new(forecast_hash, antipode_location.address)
-    antipode = Antipode.new(search_location.address, antipode_location.address, forecast)
-    antipode_json = AntipodeSerializer.new(antipode)
+    facade = AntipodeFacade.new(params[:location])
+    antipode_json = facade.return_antipode_json
     render json: antipode_json
-  end
-
-  private
-
-  def update_search_location
-    search_location = Location.find_or_create_by(address: params[:location].downcase)
-    unless search_location.latitude
-      coordinates = GoogleService.new(search_location).coordinates
-      search_location.update(latitude: coordinates[:lat], longitude: coordinates[:lng])
-    end
-    search_location
-  end
-
-  def update_antipode_location(antipode_coords)
-    antipode_location = Location.find_or_create_by(
-                latitude: antipode_coords[:data][:attributes][:lat],
-                longitude: antipode_coords[:data][:attributes][:long]
-              )
-    unless antipode_location.address
-      address = GoogleService.new(antipode_location).address
-      antipode_location.update(address: address.downcase)
-    end
-    antipode_location
   end
 end
---
+++ app/facades/antipode_facade.rb
@@ -0,0 +1,37 @@
+class AntipodeFacade
+  def initialize(address)
+    @address = address
+  end
+
+  def return_antipode_json
+    search_location = update_search_location
+    antipode_coords = AmypodeService.new(search_location).get_antipode_coords
+    antipode_location = update_antipode_location(antipode_coords)
+    forecast_hash = DarkSkyService.new(antipode_location).get_json_forecast
+    forecast = Forecast.new(forecast_hash, antipode_location.address)
+    antipode = Antipode.new(search_location.address, antipode_location.address, forecast)
+    AntipodeSerializer.new(antipode)
+  end
+
+  def update_search_location
+    search_location = Location.find_or_create_by(address: @address.downcase)
+    unless search_location.latitude
+      coordinates = GoogleService.new(search_location).coordinates
+      search_location.update(latitude: coordinates[:lat], longitude: coordinates[:lng])
+    end
+    search_location
+  end
+
+  def update_antipode_location(antipode_coords)
+    antipode_location = Location.find_or_create_by(
+                latitude: antipode_coords[:data][:attributes][:lat],
+                longitude: antipode_coords[:data][:attributes][:long]
+              )
+    unless antipode_location.address
+      address = GoogleService.new(antipode_location).address
+      antipode_location.update(address: address.downcase)
+    end
+    antipode_location
+  end
+
+end
