a83bb69e37774cb587ce1ee4bc1a64e76f8df81d
Tyler Schaffer <tpschaffer@gmail.com>
2019-11-04 10:19:19 -0700

    Update google service spec to reflect new initialize args

--- spec/services/google_service_spec.rb
+++ spec/services/google_service_spec.rb
@@ -2,17 +2,17 @@ require 'rails_helper'
 
 describe GoogleService do
   before(:each) do
-    @city = "Denver,CO"
-    @google_service = GoogleService.new(@city)
+    @location_1 = Location.create(address: "Denver,CO")
+    @google_service_1 = GoogleService.new(@location_1)
   end
 
   it "exists" do
-    expect(@google_service).to be_a(GoogleService)
+    expect(@google_service_1).to be_a(GoogleService)
   end
 
-  it "#get_json_response" do
+  it "#get_json_coords" do
     VCR.use_cassette("google_service_raw") do
-      json_response = @google_service.get_json_response
+      json_response = @google_service_1.get_json_coords
       expect(json_response[:status]).to eq "OK"
       expect(json_response[:results][0][:address_components][0][:long_name]).to eq "Denver"
     end
@@ -20,7 +20,7 @@ describe GoogleService do
 
   it "#coordinates" do
     VCR.use_cassette("google_service_coordinates") do
-      coordinates = @google_service.coordinates
+      coordinates = @google_service_1.coordinates
       expect(coordinates[:lat]).to eq 39.7392358
       expect(coordinates[:lng]).to eq -104.990251
     end
