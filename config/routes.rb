da6d7388b74930f8467a8b50d6010337fabe8e5b
Tyler Schaffer <tpschaffer@gmail.com>
2019-11-03 22:07:22 -0700

    Create route for getting background image

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      get '/forecast', to: 'forecast#show'
-      get '/background', to: 'image#show'
+      get '/forecast', to: 'forecasts#show'
+      get '/backgrounds', to: 'images#show'
     end
   end
 end
