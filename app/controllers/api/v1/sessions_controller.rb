db7e08a2ce2ee2c18c76f04ca4307b14f87b26b7
Tyler Schaffer <tpschaffer@gmail.com>
2019-11-04 21:11:05 -0700

    Change status code for successful session create

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::SessionsController < ApplicationController
     password = JSON.parse(request.body.read)["password"]
     user = User.find_by(email: email)
     if user && user.authenticate(password)
-      render json: {api_key: user.api_key}, status: 201
+      render json: {api_key: user.api_key}, status: 200
     else
       render json: {error: "User not found"}, status: 400
     end
