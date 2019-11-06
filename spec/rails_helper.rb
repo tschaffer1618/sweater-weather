34ef449d716f7fbde0e0d4a0175bfe7723d190e6
Tyler Schaffer <tpschaffer@gmail.com>
2019-11-06 09:29:34 -0700

    Filter unused files from coverage reports

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -20,7 +20,12 @@ VCR.configure do |config|
   config.filter_sensitive_data('<UNSPLASH_API_KEY>') { ENV['UNSPLASH_API_KEY'] }
 end
 
-SimpleCov.start 'rails'
+SimpleCov.start 'rails' do
+  add_filter "/app/channels/application_cable/channel.rb"
+  add_filter "/app/channels/application_cable/connection.rb"
+  add_filter "/app/jobs/application_job.rb"
+  add_filter "/app/mailers/application_mailer.rb"
+end
 
 # Add additional requires below this line. Rails is not loaded until this point!
 
