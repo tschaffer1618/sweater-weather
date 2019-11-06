baed555ae2b70379895c6f0c9a9978c3357fffbf
Tyler Schaffer <tpschaffer@gmail.com>
2019-11-06 09:37:05 -0700

    Test secondary case for image create method

--- spec/requests/api/v1/background_image_spec.rb
+++ spec/requests/api/v1/background_image_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe "GET /api/v1/backgrounds" do
-  it "provides a background image for a specific city" do
+  it "provides a new background image for a specific city" do
     VCR.use_cassette("background_image_by_location") do
       get '/api/v1/backgrounds?location=denver,co'
 
@@ -18,5 +18,32 @@ describe "GET /api/v1/backgrounds" do
       ]
       expect(data["data"]["attributes"].keys).to eq(keys)
     end
+  end
+
+  it "finds an already existing image if the location has one" do
+    location = Location.create(address: "denver,co", latitude: 39.7392358, longitude: -104.990251)
+    image = BackgroundImage.create(
+                                  location: location,
+                                  description: "Picture",
+                                  alt_description: "Pic of Denver",
+                                  raw_url: "random_image.jpg",
+                                  full_url: "full_random_image.jpg",
+                                  city: "Denver"
+                                )
+
+    get '/api/v1/backgrounds?location=denver,co'
+
+    expect(response).to be_successful
+
+    data = JSON.parse(response.body)
+    keys = [
+        "id",
+        "description",
+        "alt_description",
+        "raw_url",
+        "full_url",
+        "city"
+    ]
+    expect(data["data"]["attributes"].keys).to eq(keys)
   end
 end
