525d27c740101a5d3d23894bf115338bcddc9b40
Tyler Schaffer <tpschaffer@gmail.com>
2019-11-06 10:15:34 -0700

    Remove unnecessary local variable

--- app/facades/image_facade.rb
+++ app/facades/image_facade.rb
@@ -13,7 +13,7 @@ private
 
   def create_image(location)
     json_image = UnsplashService.new(location).get_json_image
-    image = BackgroundImage.create(location: location,
+    BackgroundImage.create(location: location,
                   description: json_image[:description],
                   alt_description: json_image[:alt_description],
                   raw_url: json_image[:urls][:raw],
