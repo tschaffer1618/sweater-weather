24a7e8104101dd731061157aa58962067822a9ce
Tyler Schaffer <tpschaffer@gmail.com>
2019-11-05 18:46:55 -0700

    Test user and session create

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,9 +1,11 @@
 class Api::V1::SessionsController < ApplicationController
   def create
-    email = JSON.parse(request.body.read)["email"]
-    password = JSON.parse(request.body.read)["password"]
+    info = JSON.parse(request.body.read)
+    email = info["email"]
+    password = info["password"]
     user = User.find_by(email: email)
     if user && user.authenticate(password)
+      session[:user_id] = user.id
       render json: {api_key: user.api_key}, status: 200
     else
       render json: {error: "User not found"}, status: 400
